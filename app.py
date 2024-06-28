4c2ca0eaf345584b96d5202a0a82c3e7335b6585
isaiale <108772859+isaiale@users.noreply.github.com>
2024-06-28 00:23:32 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,11 @@
-from flask import Flask, request, render_template, jsonify
-import pandas as pd
-from tensorflow.keras.models import load_model
+from flask import Flask, request, jsonify, render_template
 import joblib
-import logging
+import numpy as np
 
 app = Flask(__name__)
 
-# Configurar el registro
-logging.basicConfig(level=logging.DEBUG)
-
-# Cargar el modelo entrenado
-model = joblib.load('modeloStartClass.pkl')
-app.logger.debug('Modelo cargado correctamente.')
-
-# Diccionario para mapear las clases numéricas a etiquetas de texto
-class_mapping = {0: 'GALAXY', 1: 'QSO', 2: 'STAR'}
+# Cargar el modelo
+model = joblib.load('modeloConcreto.pkl')
 
 @app.route('/')
 def home():
@@ -22,35 +13,11 @@ def home():
 
 @app.route('/predict', methods=['POST'])
 def predict():
-    try:
-        # Obtener los datos enviados en el request
-        data = [
-            float(request.form['alpha']),
-            float(request.form['delta']),
-            float(request.form['u']),
-            float(request.form['g']),
-            float(request.form['r']),
-            float(request.form['i'])
-        ]
-        
-        # Crear un DataFrame con los datos
-        columns = ['alpha', 'delta', 'u', 'g', 'r', 'i']
-        data_df = pd.DataFrame([data], columns=columns)
-        app.logger.debug(f'DataFrame creado: {data_df}')
-        
-        # Realizar predicciones
-        prediction = model.predict(data_df)
-        prediction_class = prediction.argmax(axis=-1)[0]
-        app.logger.debug(f'Predicción: {prediction_class}')
-        
-        # Obtener la etiqueta de la clase predicha
-        predicted_label = class_mapping[prediction_class]
-        
-        # Devolver las predicciones como respuesta JSON
-        return jsonify({'categoria': int(prediction_class), 'label': predicted_label})
-    except Exception as e:
-        app.logger.error(f'Error en la predicción: {str(e)}')
-        return jsonify({'error': str(e)}), 400
+    data = request.get_json(force=True)
+    features = [data['cement'], data['slag'], data['flyash'], data['water'],
+                data['superplasticizer'], data['age']]
+    prediction = model.predict([features])
+    return jsonify({'prediction': prediction[0]})
 
 if __name__ == '__main__':
     app.run(debug=True)
